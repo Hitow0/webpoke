5f588758037973e1c8e14400238a71b9396b0ff5
Leo Denis <leo_denis@ens.univ-artois.fr>
2023-10-20 15:03:34 +0200

    Ajout de la première route

--- main.py
+++ main.py
@@ -7,15 +7,22 @@ from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import DataRequired
 
-
 app = Flask(__name__)
+app.secret_key = "Pokedex !"
 
 class PokemonForm(FlaskForm):
     nom = StringField("nom", validators=[DataRequired()])
 
-@app.route("/")
+
+@app.route("/index", methods=['GET', 'POST'])
 def index():
-    pass
+    pokemon_form = PokemonForm()
+    if pokemon_form.validate_on_submit():
+        response = requests.get('https://api-pokemon-fr.vercel.app/api/v1/pokemon/argouste')
+        response_data_json = response.json()
+        return render_template('index.html', adresse_json=response_data_json)
+    return render_template('index.html', form=pokemon_form)
+
 
 if __name__ == '__main__':
     app.run()
