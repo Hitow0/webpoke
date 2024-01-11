9f8446e1f9d69f9a60f8fd7f9e49879f60113fb6
Blart Nicolas <nicolas_blart@ens.univ-artois.fr>
2024-01-11 17:52:38 +0100

    :sparkles: Ajout de la page Game #31 - Ajout de l'image en noir #32 - Récupération de la réponse donnée #33 - Ajout d'un bouton pour passer #34 - Marquer si la réponse donnée est correcte ou incorrect #36

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import random
+
 import requests as requests
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, session, jsonify
 import unicodedata
 
 from flask_wtf import FlaskForm
@@ -17,6 +19,10 @@ def remove_accents(input_str):
 
 class PokemonForm(FlaskForm):
     nom = StringField("nom", validators=[DataRequired()])
+
+
+class WhosThatPokemon(FlaskForm):
+    nom = StringField('nom', validators=[DataRequired()])
 
 
 @app.route("/", methods=['GET', 'POST'])
@@ -42,6 +48,58 @@ def pokemon_info(name):
     if len(reponse_data_json) == 2:
         return render_template('pokedex.html', pokedex_json=None, form=pokedex_form, msg="Le pokemon n'existe pas.")
     return render_template('pokedex.html', pokedex_json=reponse_data_json, form=pokedex_form)
+
+
+@app.route("/game", methods=['GET', 'POST'])
+def game():
+    game_form = WhosThatPokemon()
+
+    current_pokemon_id = session.get('current_pokemon_id')
+    msg = session.get('current_msg')
+
+    if current_pokemon_id is None:
+        current_pokemon_id = obtenir_id_pokemon_aleatoire()
+        session['current_pokemon_id'] = current_pokemon_id
+
+    response = requests.get(
+        f'https://api-pokemon-fr.vercel.app/api/v1/pokemon/{current_pokemon_id}'
+    )
+    reponse_data_json = response.json()
+
+    if game_form.validate_on_submit():
+        if game_form.nom.data == reponse_data_json['name']['fr']:
+            print("Bravo ! Vous avez trouvé le Pokémon !")
+            session['current_msg'] = "Bravo ! Vous avez trouvé le Pokémon !"
+
+            # Procéder à l'ajout dans la base de données
+
+            session.pop('current_pokemon_id', None)
+        else:
+            print("Erreur !")
+            session['current_msg'] = "Erreur ! Vous n'avez pas trouvé le pokemon !"
+
+            session.pop('current_pokemon_id', None)
+        return redirect('/game')
+    else:
+        session.pop('current_msg', None)
+    return render_template('game.html', form=game_form, msg=msg, pokemon=reponse_data_json)
+
+
+@app.route('/game/skip', methods=['GET'])
+def skip():
+    session.pop('current_pokemon_id', None)
+    return redirect(url_for('game'))
+
+
+def obtenir_id_pokemon_aleatoire():
+    return random.randint(1, 1017)
+
+
+@app.route('/get_sprite_url', methods=['GET'])
+def get_sprite_url():
+    # Utilisez votre fonction pour obtenir un nouvel ID aléatoire et retournez l'URL du sprite
+    sprite_url = 'https://api-pokemon-fr.vercel.app/api/v1/pokemon/{obtenir_id_pokemon_aleatoire()}'
+    return jsonify({'sprite_url': sprite_url})
 
 
 if __name__ == '__main__':
